1828dbe2c1c79b94df7029abfbeda6591d1c868c
SMFL <Oliver-bradford@outlook.com>
2017-10-19 15:22:58 +0100

    Labs 4, and 6 uploaded

---
+++ Lab Exercise 6/game2_template/items.py
@@ -0,0 +1,65 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+    
+    "Mass": 5
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!",
+
+    "Mass": 20
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees.",
+
+    "Mass": 5
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits.",
+
+    "Mass": 5
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen.",
+
+    "Mass": 1
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously.",
+
+    "Mass": 10
+}
+
+All_Items = [item_id, item_handbook, item_pen, item_biscuits, item_money, item_laptop]
---
+++ Lab Exercise 6/game2_template/player.py
@@ -0,0 +1,9 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+
+Carry_Weight = 40
+
+# Start game at the reception
+current_room = rooms["Reception"]
